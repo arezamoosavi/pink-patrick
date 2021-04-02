483c6bcc78d6850a261c0b94bd709e09c8aabe40
alireza <sdamoosavi@gmail.com>
2021-04-03 02:31:44 +0430

    mongo presto processor added

--- container/app/run.sh
+++ container/app/run.sh
@@ -4,7 +4,23 @@
 set -o errexit
 set -o nounset
 
-sleep 10
+echo twitter stream started . . .
+
+timeout 1m python twitter_app.twitter_stream.py
+
+echo twitter stream finished
+sleep 3
+
+echo presto job is started . . .
+echo presto job from mysql to mongodb . . .
+
+
+echo is finished
+echo presto job from mysql to hive . . .
+
+
+echo is finished
+echo presto job is finished
 
 
 exec "$@"
--- src/presto_app/add_mongo.py
+++ src/presto_app/add_mongo.py
@@ -1,1 +1,13 @@
 # this script is presto job from mysql to mongodb
+
+import prestodb.dbapi as presto
+
+# conn = presto.Connection(host="presto", port=8080, user="demo")
+conn = presto.Connection(
+    host="presto", port=8080, user="demo", catalog="tcph", schema="sf10"
+)
+
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE mongodb.presto.tweets AS SELECT * FROM mysql.maindb.tweets")
+
